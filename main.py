f9326087c2589c8823cb0f08877bc366acb6441b
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-03-29 14:30:28 +0000

    Exercício resolvido!

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ idade = int(input('Digite sua idade: '))
 
 if idade < 0 :
     print('impossível!')
+
 elif idade < 18:
     print('não precisa se alistar.')
+
 elif idade >= 18 and idade <= 65:
     print('Não esqueça de votar na próxima eleição.')
 
 elif idade > 65:
-    print('Vá descançar.')
+    print('Vá descansar.')
 else:
     print('eita!')
